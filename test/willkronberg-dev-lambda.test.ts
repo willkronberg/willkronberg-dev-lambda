2c6295bb37b0fc3aab19d6043cacbe626fb74ada
Will Kronberg <will@willkronberg.dev>
2022-11-05 17:08:33 -0500

    Initial commit

---
+++ test/willkronberg-dev-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WillkronbergDevLambda from '../lib/willkronberg-dev-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/willkronberg-dev-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WillkronbergDevLambda.WillkronbergDevLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
